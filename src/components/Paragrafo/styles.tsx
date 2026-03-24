c170c94ca5a0fab17c24ab4828419d88ec32b523
Lucas Maximiano <lukas-maximiano@hotmail.com>
2026-03-24 20:59:44 -0300

    estilizando a sidebar

--- src/components/Paragrafo/styles.tsx
+++ src/components/Paragrafo/styles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Props } from './index';
 
 export const P = styled.p<Props>`
-  font-size: 14;
+  font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
   color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
   line-height: 22px;
 `;
--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar';
+import Paragrafo from '../../components/Paragrafo';
 import Titulo from '../../components/titulo';
+import { Descricao, BotaoTema, SidebarContainer } from './styles';
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/lumaximiano.png" alt="" />
-    <Titulo fontSize={20}>Lucas Maximiano</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas Maximiano</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        lumaximiano
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 );
 
